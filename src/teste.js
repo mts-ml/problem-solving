94cdb18bdc8bf2e6bea51eee33864bc797337835
Mateus <mtsmlima@gmail.com>
2025-04-04 16:46:34 -0300

    Add new excercises

--- src/teste.js
+++ src/teste.js
@@ -57,3 +57,119 @@ function timeConversion(s) {
     console.log(convertedTime)
 }
 timeConversion("12:05:45AM")
+
+
+// PROPORTIONAL RATIO
+
+function plusMinus(array) {
+    let positiveNumbers = 0
+    let neutralNumbers = 0
+    let negativeNumbers = 0
+
+    for (let num of array) {
+        if (num > 0) {
+            positiveNumbers++
+        } else if (num === 0) {
+            neutralNumbers++
+        } else {
+            negativeNumbers++
+        }
+    }
+    console.log((positiveNumbers / array.length).toFixed(6))
+    console.log((negativeNumbers / array.length).toFixed(6))
+    console.log((neutralNumbers / array.length).toFixed(6))
+}
+plusMinus([1, 2, 0, -1, -2])
+
+
+
+// REMOVING DUPLICATE LETTERS (DUPLICADOS GLOBAIS(VERIFICA O ARRAY INTEIRO))
+
+function superReducedString(s) {
+    const filteredArray = []
+    const lettersArray = s.split("")
+    console.log(lettersArray)
+
+    for (let letter of lettersArray) {
+        if (filteredArray.includes(letter)) {
+            const indexOfLetter = filteredArray.indexOf(letter)
+            filteredArray.splice(indexOfLetter, 1)
+        } else {
+            filteredArray.push(letter)
+        }
+    }
+
+
+    if (filteredArray.length > 0) {
+        const finalString = filteredArray.join("")
+        console.log(finalString)
+    } else {
+        console.log("Empty string")
+    }
+}
+
+superReducedString("zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh")
+
+// zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh
+// tqauhujtmxnsbzpykwlvpfyqijvdhuhirdmuxiobyvxupqwydkpbxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh
+
+
+// REMOVING DUPLICATE ITEMS (DUPLICADOS ADJACENTES, ESTILHO STACK(PILHA), VERIFICA O ÚLTIMO ADICIONADO.)
+
+function superReducedStrings(s) {
+    const filteredArray = []
+    const splitStringArray = s.split("")
+
+    for (let letter of splitStringArray) {
+        if (filteredArray[filteredArray.length - 1] === letter) {
+            filteredArray.pop()
+        } else {
+            filteredArray.push(letter)
+        }
+    }
+
+    if (filteredArray.length > 0) {
+        const finalString = filteredArray.join("")
+        return finalString
+    } else {
+        return "Empty String"
+    }
+}
+
+
+// Grading Students
+
+function gradingStudents(grades) {
+    const newGradesArray = []
+
+    for (let grade of grades) {
+        const roundedGrade = Math.ceil(grade / 5) * 5
+        const difference = roundedGrade - grade
+        const shouldRound = grade >= 38 && difference < 3
+
+        if (shouldRound) {            
+            newGradesArray.push(roundedGrade)
+        } else {
+            newGradesArray.push(grade)
+        }
+    }
+    console.log(newGradesArray)
+}
+gradingStudents([73, 67, 38, 33])
+
+
+
+// COUNTING WORDS IN STRING
+
+function camelcase(s) {
+    let wordCounter = 1
+
+    for (let letter of s) {
+        if (letter === letter.toUpperCase() && letter !== letter.toLowerCase()) {
+            wordCounter++
+        }
+    }
+
+    console.log(wordCounter)
+}
+camelcase("saveChangesInTheEditor")
